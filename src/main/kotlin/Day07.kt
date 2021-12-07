9ad053d5879489fde6a9c4384976cd682c26d997
Andrei Koval <kowal.andrew@gmail.com>
2021-12-07 22:06:00 +0100

    Day 07

---
+++ src/main/kotlin/Day07.kt
@@ -0,0 +1,27 @@
+import kotlin.math.abs
+
+fun main() {
+    println(Day07.solvePart1())
+    println(Day07.solvePart2())
+}
+
+object Day07 {
+    private val positions = readInput("day07")
+        .single()
+        .split(',')
+        .map { it.toInt() }
+        .sorted()
+
+    fun solvePart1(): Int {
+        val median = positions.run { (get(size / 2) + get(size / 2 + size % 2)) / 2 }
+        return positions.sumOf { abs(it - median) }
+    }
+
+    fun solvePart2(): Int {
+        fun cost(a: Int, b: Int) = abs(a - b).let { it * (it + 1) / 2 }
+        val mean = positions.average()
+        val min = (mean - 1).toInt()
+        val max = (mean + 1).toInt()
+        return (min..max).minOf { pos -> positions.sumOf { cost(it, pos) } }
+    }
+}
